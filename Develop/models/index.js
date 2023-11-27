3b2ba902d46a38e07a8f5fbcaa6db68c2985e698
AgrastP <agrastp@yahoo.com>
2023-11-27 13:57:21 -0500

    completed Models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,7 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Products.belongsTo(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'product_id',
 });
 // Categories have many Products
@@ -14,14 +14,9 @@ Category.hasMany(Product, {
   onDelete:'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag,{
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
-});
+Product.belongsToMany(Tag,{ through: 'ProductTag'});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  foreignKey: 'product_id',
-});
+Tag.belongsToMany(Product, { through: 'ProductTag'});
 
 module.exports = {
   Product,
